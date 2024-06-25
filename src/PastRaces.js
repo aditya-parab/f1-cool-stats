f77798be734b4a3ca2a7a2a4e6d40d5f7fa426ca
adparab97 <adity@Adityas-MacBook-Air-3.local>
2024-06-24 19:24:56 -0700

    pastraces, upcomingraces components all working

--- src/PastRaces.js
+++ src/PastRaces.js
@@ -1,42 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+
+
 function PastRaces() {
   const [pastRaces, setPastRaces] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const today = new Date(); // Get today's date in YYYY-MM-DD format
+  const todaysyear = today.getFullYear();
 
   async function fetchPastRaces() {
     setLoading(true);
     try {
-      const currentYear = new Date().getFullYear();
-      const url = `https://ergast.com/api/f1/${currentYear}/results.json`;
-
+      const url = `https://ergast.com/api/f1/${todaysyear}/results/1.json`;
+    
       const response = await axios.get(url);
-      const today = new Date().toISOString().slice(0, 10); // Get today's date in YYYY-MM-DD format
-      console.log(today);
+      const data = response.data?.MRData?.RaceTable?.Races;
+      setPastRaces(data);
+      console.log(data);
 
-      // Filter past races based on date
-      const filteredRaces = response.data?.MRData?.RaceTable?.Races?.filter(
-        (race) => race.date < today
-        
-      );
-      console.log(response.data?.MRData?.RaceTable?.Races);
-      setPastRaces(filteredRaces || []);
     } catch (error) {
       console.error("Error fetching past races:", error);
     } finally {
       setLoading(false);
     }
+
+
+    
   }
+
 
   useEffect(() => {
     fetchPastRaces();
   }, []);
 
   function handleRefresh() {
-    
     fetchPastRaces();
   }
+
+
+ 
+
+
 
   return (
     <div>
@@ -47,15 +52,16 @@ function PastRaces() {
       {isLoading ? (
         <div className="loading">Loading past races...</div>
       ) : pastRaces.length > 0 ? (
-        <ul>
+        <ol>
           {/* Map over pastRaces and display details */}
           {pastRaces.map((race) => (
-            <li key={race.round}>
+            <li key={race.circuit_key}>
               {/* Display race details (e.g., race name, date, winner) */}
-              {race.raceName} ({race.date}) - Winner: {race.Results[0]?.Driver?.driverId || "N/A"}
+              {race.raceName} : {race.Results[0].Driver.familyName}
+              
             </li>
           ))}
-        </ul>
+        </ol>
       ) : (
         <p>No past races found.</p>
       )}
